5fd47521258caf6aefb540ba0c8171330617a79c
Jason Chan <jasonfchan@gmail.com>
2016-05-20 05:55:20 -0700

    map functions operational

--- CampManager/CampManager/MapViewController.swift
+++ CampManager/CampManager/MapViewController.swift
@@ -8,11 +8,141 @@
 
 import MapKit
 import UIKit
+import CoreData
 
-class MapViewController: UIViewController {
+class MapViewController: UIViewController, CLLocationManagerDelegate {
+    
+    var appDelegate: AppDelegate!
+    var sharedContext: NSManagedObjectContext!
+    
+    @IBOutlet weak var mapView: MKMapView!
+    
+    var locationManager = CLLocationManager()
     
     
-    @IBOutlet weak var MapView: MKMapView!
+    @IBAction func mapType(sender: AnyObject) {
+        if mapView.mapType == MKMapType.Standard {
+            mapView.mapType = MKMapType.Satellite
+        } else {
+            mapView.mapType = MKMapType.Standard
+        }
+    }
+    @IBAction func findMeButton(sender: AnyObject) {
+        
+        let userLocation = mapView.userLocation
+        let region = MKCoordinateRegionMakeWithDistance(
+            userLocation.location!.coordinate, 2000, 2000)
+        mapView.setRegion(region, animated: true)
+    }
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+        checkLocationAuthorizationStatus()
+        
+        self.locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        
+        mapView.showsUserLocation = true
+        
+        appDelegate = UIApplication.sharedApplication().delegate as! AppDelegate
+        
+        sharedContext = appDelegate.managedObjectContext
+        
+        let longPress = UILongPressGestureRecognizer(target: self, action: "dropPin:")
+        
+        longPress.minimumPressDuration = 0.5
+        
+        mapView.addGestureRecognizer(longPress)
+        
+        mapView.addAnnotations(fetchAllPins())
+        
+        
+        
+    }
+    
+    // MARK: - Pins
+    
+    func dropPin(gestureRecognizer: UIGestureRecognizer) {
+        
+        let tapPoint: CGPoint = gestureRecognizer.locationInView(mapView)
+        let touchMapCoordinate: CLLocationCoordinate2D = mapView.convertPoint(tapPoint, toCoordinateFromView: mapView)
+        
+        if UIGestureRecognizerState.Began == gestureRecognizer.state {
+            //initialize our Pin with our coordinates and the context from AppDelegate
+            let pin = Pin(annotationLatitude: touchMapCoordinate.latitude, annotationLongitude: touchMapCoordinate.longitude, context: appDelegate.managedObjectContext)
+            //add the pin to the map
+            mapView.addAnnotation(pin)
+            
+            
+            appDelegate.saveContext()
+        }
+    }
+    
+    func mapView(mapView: MKMapView, didSelectAnnotationView view: MKAnnotationView) {
+        
+        let pin = view.annotation as! Pin
+        //delete from our context
+        sharedContext.deleteObject(pin)
+        //remove the annotation from the map
+        mapView.removeAnnotation(pin)
+        //save our context
+        appDelegate.saveContext()
+    }
+    
+    
+    func fetchAllPins() -> [Pin] {
+        
+        let error: NSErrorPointer = nil
+        // Create the Fetch Request
+        let fetchRequest = NSFetchRequest(entityName: "Pin")
+        // Execute the Fetch Request
+        let results: [AnyObject]?
+        do {
+            results = try sharedContext.executeFetchRequest(fetchRequest)
+        } catch let error1 as NSError {
+            error.memory = error1
+            results = nil
+        }
+        // Check for Errors
+        if error != nil {
+            print("Error in fectchAllActors(): \(error)")
+        }
+        // Return the results, cast to an array of Pin objects
+        return results as! [Pin]
+    }
+    
+    
+    // MARK: - Live Updates
+    func mapView(mapView: MKMapView, didUpdateUserLocation
+        userLocation: MKUserLocation) {
+        print(userLocation)
+        
+    }
+    
+    
+    
+    // MARK: - location manager to authorize user location for Maps app
+    
+    func checkLocationAuthorizationStatus() {
+        if CLLocationManager.authorizationStatus() == .AuthorizedWhenInUse {
+            mapView.showsUserLocation = true
+        } else {
+            locationManager.requestWhenInUseAuthorization()
+        }
+    }
+    
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
     
 
+    
+
+
+    
 }
---
+++ CampManager/Pin.swift
@@ -0,0 +1,17 @@
+//
+//  Pin.swift
+//  
+//
+//  Created by Jason Chan MBP on 5/19/16.
+//
+//
+
+import Foundation
+import CoreData
+
+
+class Pin: NSManagedObject {
+
+// Insert code here to add functionality to your managed object subclass
+
+}
